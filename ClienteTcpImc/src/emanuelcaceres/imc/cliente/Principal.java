1ca5a14ea7cb5fa40acccfd790c8d01b8f122fc6
Emanuel Caceres <emanuelcacerescab7779@gmail.com>
2025-03-10 08:59:37 -0500

    Proyecto subido

---
+++ ClienteTcpImc/src/emanuelcaceres/imc/cliente/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package emanuelcaceres.imc.cliente;
+
+import emanuelcaceres.imc.cliente.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author emanu
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
